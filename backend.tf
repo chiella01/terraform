8f24270c7ab284d48844b992ebe7dc64acdd519f
mentorolud <98928223+mentorolud@users.noreply.github.com>
2022-08-05 04:52:13 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
  terraform {
    backend "s3" {
-    bucket = "code-sharing-bucket"
+    bucket = "oludare-bucket-2022067"
     key    = "oludare-bucket-2022067/firstproject.tfstate"
     region = "us-east-1"
-    profile = "jespo"
+    profile = "oludare"
 dynamodb_table= "firstprojecttable"
    }
  }
