d3b0a4a6b1059356ee6a5d40677f1d021c1081a6
chiella01 <85824895+chiella01@users.noreply.github.com>
2022-10-28 21:54:16 +0100

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,10 +1,10 @@
  terraform {
    backend "s3" {
-    bucket = "oludare-bucket-2022067"
-    key    = "oludare-bucket-2022067/firstproject.tfstate"
+    bucket = "myserverlessbucket2022"
+    key    = "myserverlessbucket2022/firstproject.tfstate"
     region = "us-east-1"
-    profile = "oludare"
-dynamodb_table= "firstprojecttable"
+    profile = "chiella"
+dynamodb_table= "terraformtable"
    }
  }
 
